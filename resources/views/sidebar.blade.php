aabab12be03b7da4090b9318cc435e614337364e
Alfian Kafilah Baits <alfian.baits@gmail.com>
2026-01-16 21:54:06 +0700

    fix(sidebar): update sidebar icon and route

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,6 +7,11 @@
                     </svg>
                     <span class="font-medium">Home</span>
                 </a>
+                <!-- Belanja Redesain Menu -->
+                <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg text-gray-700 hover:bg-gray-100 transition duration-200 sidebar-active">
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M7.3 21q-.95 0-1.625-.687T5 18.675V9.4L3.175 5H1V3h3.525l1.65 4H20.95q.575 0 .875.475t.025.975L19 14.025q1.275.2 2.138 1.175T22 17.5q0 1.45-1.012 2.475T18.525 21q-1.475 0-2.487-1.025T15.025 17.5q0-.5.125-.925t.35-.825l-3.275-.3l-3 4.5q-.325.5-.837.775T7.3 21m.025-2.025q.05 0 .225-.125l2.425-3.6q-1.225-.125-1.925-.587T7 13.7v5q0 .125.1.2t.225.075M18.5 19q.65 0 1.075-.437T20 17.5q0-.65-.425-1.075T18.5 16q-.625 0-1.062.425T17 17.5q0 .625.438 1.063T18.5 19"/></svg>
+                    <span class="font-medium">Belanja Redesain</span>
+                </a>
 
                 <!-- Setting Menu with Dropdown -->
                 <div class="mt-2">
@@ -46,13 +51,7 @@
                             </svg>
                             <span>Komponen</span>
                         </a>
-                        <a href="#" class="flex items-center gap-3 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100 transition duration-200 text-sm">
-                            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 17v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.381z" clip-rule="evenodd"></path>
-                            </svg>
-                            <span>Sub Komponen</span>
-                        </a>
-                        <a href="#" class="flex items-center gap-3 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100 transition duration-200 text-sm">
+                        <a href="{{ route('akun.index') }}" class="flex items-center gap-3 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100 transition duration-200 text-sm">
                             <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 17v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.381z" clip-rule="evenodd"></path>
                             </svg>
