9bdbec00f46d28c79bf895feab51c16aa9d5ce43
Alfian Kafilah Baits <alfian.baits@gmail.com>
2026-01-16 11:43:04 +0700

    feat(login): add login function

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 
+
+// Login Routes
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 Route::get('/login', function () {
     return view('login');
 });
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
+// Dashboard Routes
+Route::resource('dashboard', DashboardController::class);
